2387cc3ada30638b4353e17617cc185b2b315574
“thukehukehukegit config --global user.email “t782465255@qq.com <782465255@qq.com>
2019-10-18 21:13:24 +0800

    python你好

--- schoolLife/src/com/schooLif/mapper/UserDao.java
+++
@@ -1,8 +0,0 @@
-package com.schooLif.mapper;
-
-import com.schoolLife.pojo.User;
-
-public interface UserDao {
-   //用户头像保存添加
-	public void addUser(User user);
-}
--- schoolLife/src/com/schoolLife/service/FanKuiService.java
+++
@@ -1,8 +0,0 @@
-package com.schoolLife.service;
-
-import com.schoolLife.pojo.FanKui;
-
-public interface FanKuiService {
-	//反馈信息添加
-    public void addfanKui(FanKui fanKui);
-}
